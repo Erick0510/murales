8d68180e41d2f3875271033fa7da5bb2717e3042
Erick <erick_3041210064@utd.edu.mx>
2024-03-19 21:51:32 -0600

    v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-// import './App.css'
+import './App.css'
 import { Excalidraw } from "@excalidraw/excalidraw";
 
 function App() {
-  
-
   return (
     <>
       <h1 style={{ textAlign: "center" }}>Excalidraw Example</h1>
-      <div style={{ height: "500px" }}>
+      <div style={{ height: "500px", width: "1400px"}}>
         <Excalidraw />
       </div>
     </>
