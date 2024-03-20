85b3c584a5d1765346af7ece14ca347217ce49e0
Erick <erick_3041210064@utd.edu.mx>
2024-03-19 21:08:03 -0600

    v1

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+import { Excalidraw } from "@excalidraw/excalidraw";
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 style={{ textAlign: "center" }}>Excalidraw Example</h1>
+      <div style={{ height: "500px" }}>
+        <Excalidraw />
+      </div>
+    </>
+  )
+}
+
+export default App
