149e6f725d6041ac84406d5d2da8d4de7a957500
Erick <erick_3041210064@utd.edu.mx>
2024-03-21 21:39:05 -0600

    v3

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,64 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import { Excalidraw } from "@excalidraw/excalidraw";
+import { Excalidraw, LiveCollaborationTrigger,  } from "@excalidraw/excalidraw";
+import { defaultLang } from "@excalidraw/excalidraw";
+
 
 function App() {
+ 
+  const [excalidrawAPI, setExcalidrawAPI] = useState(null);
+  const [isCollaborating, setIsCollaborating] = useState(false);
+
+  defaultLang.code = "es-ES";
+  defaultLang.label = "Español";
+  
+  
   return (
     <>
-      <h1 style={{ textAlign: "center" }}>Excalidraw Example</h1>
-      <div style={{ height: "500px", width: "1400px"}}>
-        <Excalidraw />
+     <div className="app-container">
+      <div className="excalidraw-container">  
+        <label style={{ fontSize: "16px", fontWeight: "bold" }}>
+          <input
+            type="checkbox"
+            checked={isCollaborating}
+            onChange={() => {
+              if (!isCollaborating) {
+                const collaborators = new Map();
+                collaborators.set("id1", {
+                  username: "Doremon",
+                  avatarUrl: "../../../../img/doremon.png",
+                });
+                collaborators.set("id3", {
+                  username: "Pika",
+                  avatarUrl: "../../../../img/pika.jpeg",
+                });
+                excalidrawAPI.updateScene({ collaborators });
+              } else {
+                excalidrawAPI.updateScene({
+                  collaborators: new Map(),
+                });
+              }
+              setIsCollaborating(!isCollaborating);
+            }}
+          />
+          Show Collaborators
+        </label>
+        <Excalidraw
+          ref={(api) => setExcalidrawAPI(api)}
+          renderTopRightUI={() => (
+            <LiveCollaborationTrigger
+              isCollaborating={isCollaborating}
+              onSelect={() => {
+                window.alert("You clicked on collab button");
+                setIsCollaborating(true);
+              }}
+            />
+          )}
+        />
       </div>
+    </div>
     </>
   )
 }
